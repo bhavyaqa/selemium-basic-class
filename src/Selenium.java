03205a0d1e1e6729e0fa6b3b6428094a89bf2454
bhavyaqa <40259301+bhavyaqa@users.noreply.github.com>
2018-06-19 14:26:05 +0530

    all tatoc basics

--- src/Selenium.java
+++ src/Selenium.java
@@ -1,14 +1,71 @@
+
+import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 public class Selenium {
 	public static void main (String args[]) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\bhavyakhanna\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("http://10.0.1.86/tatoc/basic/grid/gate");
-		WebElement ClickGreen = driver.findElement(By.className("greenbox"));
-		ClickGreen.click();
-		driver.quit();
+		driver.get("http://10.0.1.86/tatoc");
+		//Test Automation Training Obstacle Course
+		driver.findElement(By.tagName("a")).click();
+		//grid gate
+		driver.findElement(By.className("greenbox")).click();
+		//frame dungeon
+		driver.switchTo().frame(driver.findElement(By.id("main")));
+		WebElement box1 = driver.findElement(By.id("answer"));
+		String color1 = box1.getAttribute("class");
+		driver.switchTo().frame(driver.findElement(By.id("child")));
+		WebElement box2 = driver.findElement(By.id("answer"));
+		String color2 = box2.getAttribute("class");
+		while(!color1.equals(color2)) {
+			 driver.switchTo().defaultContent();
+			 driver.switchTo().frame(driver.findElement(By.id("main")));
+			 driver.findElement(By.tagName("a")).click();
+			 driver.switchTo().frame(driver.findElement(By.id("child")));
+			 box2 = driver.findElement(By.id("answer"));
+			 color2 = box2.getAttribute("class");
+		}
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame(driver.findElement(By.id("main")));
+		List<WebElement> anchors = driver.findElements(By.tagName("a"));
+		
+		anchors.get(anchors.size()-1).click();
+		// Drag around
+		WebElement from = driver.findElement(By.id("dragbox"));
+		WebElement to = driver.findElement(By.id("dropbox"));
+        Actions act=new Actions(driver);
+        act.dragAndDrop(from, to).build().perform();
+        driver.findElement(By.tagName("a")).click();
+        //popup windows
+        List<WebElement> popup = driver.findElements(By.tagName("a"));
+        popup.get(0).click();
+        String originalwindow = driver.getWindowHandle();
+        for (String handle : driver.getWindowHandles()) 
+        {
+        driver.switchTo().window(handle); 
+        
+        }
+    	driver.findElement(By.id("name")).sendKeys("Bhavya");
+    	driver.findElement(By.id("submit")).click();
+    	driver.switchTo().window(originalwindow);
+        popup.get(1).click();
+        //cookie handling
+        List<WebElement> CookieHandlingLinks = driver.findElements(By.tagName("a"));
+        CookieHandlingLinks.get(0).click();
+        
+        String token = driver.findElement(By.id("token")).getText();
+        String newtoken = token.replace("Token: ","");
+		Cookie tatoc_cookie = new Cookie("Token",newtoken);
+        driver.manage().addCookie(tatoc_cookie);
+        
+        CookieHandlingLinks.get(1).click();
+
+        
+		
 	}
 }
